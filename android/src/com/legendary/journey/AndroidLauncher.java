09e5dee5827e8522d34f37a3d39def391a778046
Daniel Simões <daniel.simes@wit-software.com>
2018-02-12 15:09:58 +0000

    1st commit

---
+++ android/src/com/legendary/journey/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.legendary.journey;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.legendary.journey.LegendaryJourney;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LegendaryJourney(), config);
+	}
+}
